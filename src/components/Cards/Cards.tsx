c76f4baf21fab9bb19e5941440ad58a37ab9cd24
MariStakhovskaya <maryia.jegorova@gmail.com>
2021-07-03 11:15:39 +0300

    refactor fetch2

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -277,7 +277,7 @@ const Cards = () => {
     console.log(cardsQuestions)
     useEffect( () => {
 
-    fetch('http://https://maristakhovskaya.github.io/workbook/db.json/react')
+    fetch('https://my-json-server.typicode.com/MariStakhovskaya/workbook/db')
     .then(res => {
         return res.json()
     })
